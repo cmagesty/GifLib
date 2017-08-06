8949d57e937de3465068cdfaaefae7b0d66b669a
Chris Magesty <cmagesty@gmail.com>
2017-08-06 17:53:05 -0400

    1

---
+++ src/main/java/com/giflib/controller/GifController.java
@@ -0,0 +1,19 @@
+package com.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+    @RequestMapping("/")
+    public String  listGifs() {
+        return "home";
+    }
+
+    @RequestMapping("/gifs")
+    @ResponseBody
+    public String gif() {
+        return "String of my choice";
+    }
+}
